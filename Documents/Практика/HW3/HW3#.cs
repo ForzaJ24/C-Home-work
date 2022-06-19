6bd69ff44682bf35fddd4eee1e412929ffe582b7
Evgeniy Kuznetsov <kuznetsovevgeniyolegovich@mail.ru>
2022-06-19 15:26:59 +0300

    pre work - first action

---
+++ Documents/Практика/HW3/HW3#.cs
@@ -0,0 +1,5 @@
+﻿// принимает на вход число, определяет день недели и выходной ли это день
+Console.WriteLine("Hello! Enter number of day :");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<6)
+      Console.WriteLine("Go to the work!");
