eea42a0c008f0d1a2a3f5b2bcb4aad7fd245e96c
Evgeniy Kuznetsov <kuznetsovevgeniyolegovich@mail.ru>
2022-06-21 11:33:37 +0300

    pre work - first action

---
+++ Documents/Практика/Семинар2-Задача2/Задача2.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// слева заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Enter the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("Wrong number!");
+else
+{
+     while (n > 999) ;
+     n = n / 10;
+n = n % 100;
+Console.WriteLine("Third number is" + n);
+}
